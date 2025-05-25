266b619217e054bb1e8037cd56645e8fc86b9c08
Svetlana <pl.swetik@yandex.ru>
2025-05-25 23:53:18 +0400

    Удалена строка <ImplicitUsings>enable</ImplicitUsings>

---
+++ PhonebookLibrary/Abonent.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace PhonebookLibrary
+{
+    public class Abonent
+    {
+        public string Name { get; }
+        public string Phone { get; set; }
+        public string Description {  get; set; }
+        public Abonent(string name, string phone)
+        {
+            this.Name = name;
+            this.Phone = phone;
+        }
+    }
+}
